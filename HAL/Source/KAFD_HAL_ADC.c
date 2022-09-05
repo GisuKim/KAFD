27c36fbe95b0a178c11cb47158cb42e466d9fff7
gisu kim <kskwinner@gmail.com>
2022-09-06 04:26:33 +0900

    시험 버전

--- HAL/Source/KAFD_HAL_ADC.c
+++ HAL/Source/KAFD_HAL_ADC.c
@@ -18,8 +18,8 @@ void ConfigureADC(void)
 
     //
     // Write configurations
-    //
-    AdcaRegs.ADCCTL2.bit.PRESCALE = 6; //set ADCCLK divider to /4   50Mhz
+    // 현재 System Clock은 200Mhz
+    AdcaRegs.ADCCTL2.bit.PRESCALE = 6; //set ADCCLK divider to /4   50Mhz  <--- 분해능
     AdcSetMode(ADC_ADCA, ADC_RESOLUTION_12BIT, ADC_SIGNALMODE_SINGLE);
 
     //
